db9448164fa1020378614e494e2d0fb19d1c46c3
samiam567 <35432488+samiam567@users.noreply.github.com>
2018-03-28 23:51:13 -0400

    V4.7.1

--- physics_engine/src/Physics_engine/Settings.java
+++ physics_engine/src/Physics_engine/Settings.java
@@ -4,7 +4,7 @@ import java.awt.Color;
 
 public class Settings {
 	
-	public static final String version = "4.7.0";
+	public static final String version = "4.7.1";
 	
 	
 	public static int width = 1500;
